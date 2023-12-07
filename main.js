ab093b213bfb1c123f2e20080eae237dd31e7c9c
abuhosen <abu.hosen195@gmail.com>
2023-12-08 00:17:26 +0600

    add some javascript code to animation

---
+++ main.js
@@ -0,0 +1,8 @@
+const animatedSection = document.getElementById("animatedSection");
+
+window.addEventListener("scroll", animation);
+
+function animation() {
+  const triggerBottom = window.innerHeight;
+}
+animation();
